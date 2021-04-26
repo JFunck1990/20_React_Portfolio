aa84bb7c529851c830e63c0a26f4d255ca4edfc0
Jack Funck <jackfunck21@gmail.com>
2021-04-26 13:01:21 -0400

    finished the nav bar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,39 +2,21 @@ import React from "react";
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a className="nav-item nav-link active" href="#">
-            Home <span class="sr-only">(current)</span>
-          </a>
-          <a className="nav-item nav-link" href="#">
-            Features
-          </a>
-          <a className="nav-item nav-link" href="#">
-            Pricing
-          </a>
-          <a className="nav-item nav-link disabled" href="#">
-            Disabled
-          </a>
-        </div>
-      </div>
-    </nav>
-  );
+    <nav class="nav content-center navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Jack Funck</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="justify-content-center collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Portfolio</a>
+      <a class="nav-item nav-link" href="#">Resume</a>
+      <a class="nav-item nav-link" href="#">Contact</a>
+    </div>
+  </div>
+</nav>
+  )
 }
 
 export default Header;
